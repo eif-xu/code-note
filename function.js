54c654b8c79969aed42d0db405a7b9671d5ae689
eif-xu <eifxmq@gmail.com>
2017-05-05 22:29:07 +0800

    bushu

--- function.js
+++ function.js
@@ -1,114 +1,142 @@
- /* function型，作为 对象 返回
-
- function a(){return a=1};
- //console.log(typeof(a))
-
- var d=function(){
- 	return a;
- }
- d();
-
- console.log(a)
- *
- */
-
- //作为对象必然会有属性--常见属性arguments
- // function box(){
- // 	return arguments.length;
- // }
- // console.log(box(1,2,3,4))
-
-// var a=[]
-// for(var k=0;k<10000;k++){
-// 	a[k]=k;
+// 封装
+// var Book = function(name){
+// this.name = name;
 // }
-// console.log(a.length);
+// Book.prototype.getName = function(){console.log(this.name)}
 
-
-
-
-//console.time('定时器1');
- // function box(){
- // 	var sum=0;
- // 	if (arguments.length===0){return '输入大于0'}
- // 	for (var i=0;i<arguments.length;i++) {
- // 		sum+=arguments[i]
- // 	}
- // 	return sum;
- // }
- //console.log(box(a))
- //console.timeEnd('定时器1');
-
-
-//console.time('定时器2')
- // function box2(){
- // 	var sum2=0;
- // 	var i=0;
- // 	if (arguments.length===0){return '输入大于0'}
- // 	for(var i in arguments){
- // 		sum2+=arguments[i];
- // 	}
- // 	return sum2;
- // }
- //console.log(box2(a));
- // console.timeEnd('定时器2');
-
-
-
-// this相当于指针，指向函数执行域，this相当于上一级对象
-//window.color='red';
-//console.log(this.color);
-
-// var box={
-// 	color:'lanse',
-// 	printcolor: function(){
-// 		return this.color
-// 	}
+// var TechBook = function(name){
+// Book.call(this, name);
 // }
-// console.log(box.printcolor())
-// function box(num1,num2){
-// 	return num1+num2;
+// TechBook.prototype = new Book(); 
+
+// var tb = new TechBook('js');
+// tb.getName();
+
+
+
+
+
+// function SuperRobot(data) {
+//     var say = function() { return "Hello World!"; };
+//     say.prototype.age=data.age;
+//     console.log(say.age)
+//     return say;
 // }
-// function box2(num1,num2){
-// 	return box.apply(this,arguments)
+// console.log(super_robot.age)
+
+
+
+
+//一样的方法与属性都放在原型中，不同的属性与方法写到构造函数中
+
+
+
+
+
+// 关于非构造函数实现继承
+// var chinese={nation:'中国'}
+// function ob(o){
+// 	var f=function(){};
+// 	f.prototype=o;
+// 	return new f();
 // }
-//console.log(box2(10,15))
+// var Doctor=ob(chinese);
+// Doctor.career='医生';
+// console.log(Doctor.nation)
 
-// var box='sole'
-// console.log(box instanceof String)
 
 
-// function box(obj){
-// 	obj.name='les';
-// 	var obj.name='no';
+
+
+
+
+
+
+
+// ajax的回调函数的使用
+// function fn(url, callback){
+//     var httpRequest;　　　　        //创建XHR
+//     httpRequest = window.XMLHttpRequest ? new XMLHttpRequest() : window.ActiveXObject ? new ActiveXObject("Microsoft.XMLHTTP") : undefined;//针对IE进行功能性检测
+
+//     httpRequest.onreadystatechange = function(){
+//       if(httpRequest.readystate === 4
+//                 && httpRequest.status === 200){　　//状态判断
+//           callback.call(httpRequest.responseXML);
+//        }
+//     };
+//     httpRequest.open("GET", url);
+//     httpRequest.send();
 // }
-// console.log(obj.name);
+
+// fn("http://www.codefordream.com/ajax_data.xml", function(){　　　　//调用函数
+//    console.log("content:"+this); 　　//此语句后输出
+// });
+
+// console.log("this will run before the above callback.")
 
 
-// var box='jhl';
-// function setbox(){
-// 	function setbox2(){
-// 		var b='dsa';
-// 		console.log(box);
-// 	}
-// 	console.log(b)
-// }
-// setbox();
-//console.log(b)
-// for(var i=0;i<=5;i++){
-// 	if(true){
-// 		var num1=1,num2=0;
-// 		num1+=num2;
-// 	}
-// }
-// console.log(num1);
+/*
+*
+*var box=new Object;
+*box.name='xx';
+*box.age=12;
+*box.run=function(){return this.name+this.age}
+*console.log(box.run())
+*
+*/
+
+/*
+*
+*构造函数法改进于工厂模式
+*function Box(name,age){
+*	this.name=name;
+*	this.age=age;
+*	this.run=function(){return '名字'+this.name+'年龄'+this.age}
+*}
+
+*var box1= new Box('hgj',3);
+*var box2= new Box('qwe',32);
+
+*console.log(box1.run())
+*console.log(box2.run())
+*
+*/
+
+// function Box(){};
+// Box.prototype.name='xxx';
+// Box.prototype.age=12;
+
+// var box1=new Box;
+// console.log(box1.name);
 
 
-// console.log(Math.random()*100)
+/*
+*
+ *动态原型模式
+*function Box(name,age){
+*	this.name=name;
+*	this.age=age;
+*
+*	if(typeof(this.run)!='function'){
+*		Box.prototype.run=function(){return this.name+this.age+'运行中'}
+*	}
+*}
+*var box=new Box('xxx',12);
+*console.log(box.run())
+*
+*/
 
-this一定是在function运行时进行赋值
 
-apply()，第二个参数既传入实际参数，一定要用[].
+寄生构造模式        =工厂模式+构造函数模式
 
-
+function Box(name,age){
+	var obj=new Object();
+	obj.name=name;
+	obj.age=age;
+	obj.run=function(){
+		return this.name+'运行中' 
+	}
+	return obj
+}
+var box1=new Box('21',43)
 
